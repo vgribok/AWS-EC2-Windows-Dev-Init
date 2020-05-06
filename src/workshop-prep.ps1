e8842d346f9f204a212894067dac3fdfee89f2ff
Vlad Hrybok <vgribok@hotmail.com>
2020-05-06 17:59:29 -0400

    Adding diag output for the AWS SDK "default" profile.

--- src/workshop-prep.ps1
+++ src/workshop-prep.ps1
@@ -185,6 +185,7 @@ function InitWorkshop {
 
     # KLUDGE ALERT: Re-initializing AWS SDK "default" credentials profile in atempt to work around the missing SDK "default" profile
     Set-AWSCredential -AccessKey (aws configure get aws_access_key_id) -SecretKey (aws configure get aws_secret_access_key) -StoreAs default
+    Get-AWSCredential -ListProfileDetail
 
     $now = get-date
     "Workshop dev box initialization has finished on $now" 
