f4eb63a457e17fc196f6d0497136ce07172f450c
Vlad Hrybok <vgribok@hotmail.com>
2020-05-19 20:02:37 -0400

    A bit more dig output in the user session init script

--- src/aws-workshop-on-user-login.ps1
+++ src/aws-workshop-on-user-login.ps1
@@ -7,6 +7,10 @@ Import-Module awspowershell.netcore
 function InitUserSession {
     # Copy AWS CLI credentials to AWS SDK credentials store
     Set-AWSCredential -AccessKey (aws configure get aws_access_key_id --profile default) -SecretKey (aws configure get aws_secret_access_key --profile default) -StoreAs default
+    
+    # Diagnostic output
+    Write-Information "Retrieving current AWS SDK credentials `"default`" profile:"
+    Get-AWSCredential -ProfileName default
 }
 
 InitUserSession *> C:\aws-workshop-user-session-init.log
