69cc21ada4305666afeef6d5604214ec9848c6c8
Vlad Hrybok <vgribok@hotmail.com>
2020-05-19 21:54:20 -0400

    Fixed a long-standing bug in the initialize-workshop.bat file.

--- src/initialize-workshop.bat
+++ src/initialize-workshop.bat
@@ -1,4 +1,6 @@
 pwsh.exe -File c:\aws-ec2-lab-dev-box-bootstrap.ps1 -redirectToLog "redirect"
 cd "%USERPROFILE%\AWS-EC2-Windows-Dev-Init\src"
-copy /Y aws-ec2-lab-dev-box-bootstrap.ps1 aws-workshop-user-session-bootstrap.ps1 *.bat c:\*.*
+copy /Y aws-ec2-lab-dev-box-bootstrap.ps1 c:\*.*
+copy /Y aws-workshop-user-session-bootstrap.ps1 c:\*.*
+copy /Y *.bat c:\*.*
 cd /
