2d27e7771c11deb7b96a6f186e37ac5787b68e63
Vlad Hrybok <vgribok@hotmail.com>
2020-05-19 20:00:18 -0400

    Added logging to user session init script

--- src/aws-workshop-on-user-login.ps1
+++ src/aws-workshop-on-user-login.ps1
@@ -4,5 +4,9 @@ This script is invoked by Windows Startup folder each time suer logs in
 
 Import-Module awspowershell.netcore
 
-# Copy AWS CLI credentials to AWS SDK credentials store
-Set-AWSCredential -AccessKey (aws configure get aws_access_key_id --profile default) -SecretKey (aws configure get aws_secret_access_key --profile default) -StoreAs default
+function InitUserSession {
+    # Copy AWS CLI credentials to AWS SDK credentials store
+    Set-AWSCredential -AccessKey (aws configure get aws_access_key_id --profile default) -SecretKey (aws configure get aws_secret_access_key --profile default) -StoreAs default
+}
+
+InitUserSession *> C:\aws-workshop-user-session-init.log
